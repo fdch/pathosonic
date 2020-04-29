aac8c030e3147a9390db0bbcd296706a77ab8e86
Fede Camara Halac <camarafede@gmail.com>
2020-04-29 14:17:05 -0400

    added new videos and images

--- poster/make_html.sh
+++ poster/make_html.sh
@@ -1,1 +1,16 @@
+#!/bin/sh
+
+# Convert to html
 pandoc -f latex -t html -s pathosonic.tex --css pandoc.css --bibliography=pathosonic.bib -o index.html
+
+# See html file
+open index.html
+
+# Make pdf
+pdflatex -interaction=nonstopmode pathosonic.tex > /tmp/log
+bibtex pathosonic > /tmp/biblog
+pdflatex -interaction=nonstopmode pathosonic.tex > /tmp/log
+pdflatex -interaction=nonstopmode pathosonic.tex > /tmp/log
+
+echo "cat /tmp/{log,biblog} for latex compilation errors and warnings"
+exit
