ee8710aec78ee70beb167d69f0da82300ad1c318
Fede Camara Halac <camarafede@gmail.com>
2020-06-08 12:20:38 -0400

    4-page reduction to meet with NIME guidelines

--- poster/make_html.sh
+++ poster/make_html.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
+if [[ $1 ]]
+then
+	# Convert to html
+	pandoc -f latex -t html -s pathosonic.tex --css pandoc.css --bibliography=pathosonic.bib -o index.html
 
-# Convert to html
-pandoc -f latex -t html -s pathosonic.tex --css pandoc.css --bibliography=pathosonic.bib -o index.html
-
-# See html file
-open index.html
-
+	# See html file
+	open index.html
+else 
+	echo "Making pdf only. Pass any argument to convert to html."
+fi
 # Make pdf
 pdflatex -interaction=nonstopmode pathosonic.tex > /tmp/log
 bibtex pathosonic > /tmp/biblog
